aa8e1fc8940f02a1bf818274fdc18a6dddd2bc77
bahtirek <bahtirek@gmail.com>
2023-03-12 23:10:50 -0400

    added active state for active apge

--- src/app/extension/test-case/test-case.module.ts
+++ src/app/extension/test-case/test-case.module.ts
@@ -19,9 +19,6 @@ const routes: Routes = [
       {
         path: 'create', component: CreateTestCaseComponent
       },
-      /* {
-        path: '', component: CreateTestCaseComponent
-      }, */
     ] 
   }
 ];
