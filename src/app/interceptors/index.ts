cd43f9ff3a86380e7f8deb837f754fea454bf008
bahtirek <bahtirek@gmail.com>
2023-04-07 23:53:25 -0400

    auth interceptor

--- src/app/interceptors/index.ts
+++ src/app/interceptors/index.ts
@@ -2,10 +2,10 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { ErrorInterceptor } from './error.interceptor';
 import { LoaderInterceptor } from './loader.interceptor';
+import { AuthInterceptor } from './auth.interceptor';
 /* import { NotifyInterceptor } from './notify.interptor';
 import { HttpsInterceptor } from './https.interceptor';
 import { ProfilerInterceptor } from './profiler.interceptor';
-import { AuthInterceptor } from './auth.interceptor';
 import { CacheInterceptor } from './cache.interceptor';
 import { HeaderInterceptor } from './header.interceptor';
 import { FakeInterceptor } from './fake.interceptor';
@@ -14,8 +14,8 @@ import { ConvertInterceptor } from './convert.interceptor'; */
 export const httpInterceptorProviders = [
   { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true },
-  /* { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-  { provide: HTTP_INTERCEPTORS, useClass: CacheInterceptor, multi: true },
+  { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+  /* { provide: HTTP_INTERCEPTORS, useClass: CacheInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: ConvertInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: FakeInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: HeaderInterceptor, multi: true },
