7851ad8ed6e61a2df60bd9df785090c7df7a1e94
bahtirek <bahtirek@gmail.com>
2023-04-08 22:45:30 -0400

    ng0100 error fix

--- src/app/extension/extension.component.ts
+++ src/app/extension/extension.component.ts
@@ -1,21 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, AfterContentChecked } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { LoaderService } from '../shared/loader/loader.service';
-import { ToasterService } from '../shared/toaster/toaster.service';
 
 @Component({
   selector: 'app-extension',
   templateUrl: './extension.component.html',
   styleUrls: ['./extension.component.less']
 })
-export class ExtensionComponent implements OnInit {
+export class ExtensionComponent implements OnInit, AfterContentChecked {
 
   loading = this.loader.isLoading;
 
-  constructor(private router: Router, private route: ActivatedRoute, private loader: LoaderService) { }
+  constructor(private router: Router, private route: ActivatedRoute, private loader: LoaderService, private cd: ChangeDetectorRef) { }
 
   ngOnInit(): void {
     this.router.navigate(['test-case'], { relativeTo: this.route, skipLocationChange: true });
-    console.log(this.route.snapshot.url);
+  }
+
+  ngAfterContentChecked(): void {
+    this.cd.detectChanges();
   }
 }
