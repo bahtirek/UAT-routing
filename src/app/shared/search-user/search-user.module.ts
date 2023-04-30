4422d9e402f58e32a8534fb0996d0b696d1bd056
bahtirek <bahtirek@gmail.com>
2023-04-30 12:44:55 -0400

    search user component

--- src/app/shared/search-user/search-user.module.ts
+++ src/app/shared/search-user/search-user.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchUserComponent } from './search-user.component';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -9,7 +10,8 @@ import { SearchUserComponent } from './search-user.component';
     SearchUserComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports: [
     SearchUserComponent
